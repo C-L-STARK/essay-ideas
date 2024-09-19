3be4ceaf288c449bb236ae66ee61be100ef89726
StarkDylan <x.stark.dylan@gmail.com>
2024-09-19 17:42:37 +0800

    modify site map generate

--- .i18nrc.js
+++ .i18nrc.js
@@ -13,10 +13,10 @@ module.exports = defineConfig({
   modelName: "gpt-4",
   markdown: {
     reference:
-      "Translate the following content into the target language while preserving the original formatting and specific elements. For MDX files, ensure that markdown syntax, URLs, code blocks, and other formatting elements such as links remain unchanged. Do not alter the structure or content within these elements. keeping all other parts, including URLs and code structure, exactly as they are. translate systemPrompt's value in the mdx component. .",
-    entry: ["./pages/category/cause_and_effect/templates.en-US.mdx"],
+      "Translate the following content into the target language while preserving the original formatting and specific elements. For MDX files, ensure that markdown syntax, URLs, code blocks, and other formatting elements such as links remain unchanged. Do not alter the structure or content within these elements. keeping all other parts, including URLs and code structure, exactly as they are. translate systemPrompt's value in the mdx component. do not translate the first import line",
+    entry: ["./pages/category/cause_and_effect/*.en-US.mdx"],
     entryLocale: "en-US",
-    outputLocales: ["zh-CN"],
+    outputLocales: ["de-DE", "ja-JP", "ar-SA"],
     // exclude: ["./contributing/_Sidebar.mdx"],
     outputExtensions: (locale, { filePath }) => {
       if (filePath.includes(".mdx")) {
