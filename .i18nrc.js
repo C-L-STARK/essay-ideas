54888f43ecbde6a8da18f853bde6c7c91d6af282
StarkDylan <x.stark.dylan@gmail.com>
2024-09-19 11:32:46 +0800

    Finish index page

--- .i18nrc.js
+++ .i18nrc.js
@@ -14,7 +14,7 @@ module.exports = defineConfig({
   markdown: {
     reference:
       "Translate the following content into the target language while preserving the original formatting and specific elements. For MDX files, ensure that markdown syntax, URLs, code blocks, and other formatting elements such as links remain unchanged. Do not alter the structure or content within these elements. keeping all other parts, including URLs and code structure, exactly as they are. For JSON files, only translate the value associated with the key 'title', keeping all other parts, including URLs and code structure, exactly as they are.",
-    entry: ["./pages/skills/*.zh-CN.mdx"],
+    entry: ["./pages/team.zh-CN.mdx"],
     entryLocale: "zh-CN",
     outputLocales: outputLocals,
     // exclude: ["./contributing/_Sidebar.mdx"],
