3b235e983aef9215ce692d9dfa7aa71baa9a17db
StarkDylan <x.stark.dylan@gmail.com>
2024-09-19 20:19:45 +0800

    Add vercel analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import { Analytics } from "@vercel/analytics/react";
 
 export default class MyDocument extends Document {
   render() {
@@ -24,6 +25,7 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <Analytics />
         </body>
       </Html>
     );
