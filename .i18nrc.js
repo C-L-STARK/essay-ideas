c8ec7a1df8532d30646e7bffa61d653494b61e64
StarkDylan <x.stark.dylan@gmail.com>
2024-09-19 17:50:10 +0800

    add new i18n files and update sitemap config

--- .i18nrc.js
+++ .i18nrc.js
@@ -10,13 +10,13 @@ module.exports = defineConfig({
   reference:
     "For JSON files, Keep Original Key Order!!! only translate the value associated with the key 'title', keeping all other parts, including URLs and code structure, exactly as they are.",
   temperature: 0,
-  modelName: "gpt-4",
+  modelName: "gpt-3.5-turbo",
   markdown: {
     reference:
       "Translate the following content into the target language while preserving the original formatting and specific elements. For MDX files, ensure that markdown syntax, URLs, code blocks, and other formatting elements such as links remain unchanged. Do not alter the structure or content within these elements. keeping all other parts, including URLs and code structure, exactly as they are. translate systemPrompt's value in the mdx component. do not translate the first import line",
     entry: ["./pages/category/cause_and_effect/*.en-US.mdx"],
     entryLocale: "en-US",
-    outputLocales: ["de-DE", "ja-JP", "ar-SA"],
+    outputLocales: ["ja-JP", "ar-SA"],
     // exclude: ["./contributing/_Sidebar.mdx"],
     outputExtensions: (locale, { filePath }) => {
       if (filePath.includes(".mdx")) {
--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,5 +2,28 @@
 module.exports = {
   siteUrl: "https://essay-ideas.com",
   generateRobotsTxt: true, // (optional)
-  // ...other options
+  // Default transformation function
+  transform: async (config, path) => {
+    let realPath = path;
+
+    if (path.endsWith(".zh-CN")) {
+      realPath = "/zh-CN" + path.replace(".zh-CN", "");
+    }
+
+    if (path.endsWith(".ja-JP")) {
+      realPath = "/ja-JP" + path.replace(".ja-JP", "");
+    }
+
+    if (path.endsWith(".de-DE")) {
+      realPath = "/de-DE" + path.replace(".de-DE", "");
+    }
+
+    if (path.endsWith(".ar-SA")) {
+      realPath = "/ar-SA" + path.replace(".ar-SA", "");
+    }
+
+    return {
+      loc: realPath, // => this will be exported as http(s)://<config.siteUrl>/<path>
+    };
+  },
 };
