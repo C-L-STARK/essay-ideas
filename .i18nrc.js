b55c035db959f063f9e73ed2f27e964efa3ae4ae
StarkDylan <x.stark.dylan@gmail.com>
2024-09-22 10:27:34 +0800

    Add some examples

--- .i18nrc.js
+++ .i18nrc.js
@@ -14,7 +14,7 @@ module.exports = defineConfig({
   markdown: {
     reference:
       "Translate the following content into the target language while preserving the original formatting and specific elements. For MDX files, ensure that markdown syntax, URLs, code blocks, and other formatting elements such as links remain unchanged. Do not alter the structure or content within these elements. keeping all other parts, including URLs and code structure, exactly as they are. translate systemPrompt's value in the mdx component. do not translate the first import line. do not translate the marked words.",
-    entry: ["./pages/category/**/*.en-US.mdx"],
+    entry: ["./pages/examples/**/*.en-US.mdx"],
     entryLocale: "en-US",
     outputLocales: ["zh-CN", "de-DE", "ja-JP", "ar-SA"],
     // exclude: ["./contributing/_Sidebar.mdx"],
